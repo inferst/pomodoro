e46eb456d94bc81cfaf9334e35261e1bdb6be151
Mikhail Morozov <inferst@gmail.com>
2023-10-28 14:01:39 +0300

    another try

--- src/settings/connection.ts
+++ src/settings/connection.ts
@@ -22,7 +22,7 @@ export enum PomodoroStatus {
 }
 
 export const connection = (onUpdate: (state: PomodoroState) => void) => {
-  const socket = io('http://pomodoro.mikedanagam.space/api');
+  const socket = io('http://pomodoro.mikedanagam.space', {path: 'api'});
 
   const url = new URLSearchParams(location.search);
   const roomId = url.get('room');
--- src/timer/connection.ts
+++ src/timer/connection.ts
@@ -15,7 +15,7 @@ export enum PomodoroStatus {
 }
 
 export const connection = (onUpdate: (state: PomodoroState) => void) => {
-  const socket = io('http://pomodoro.mikedanagam.space/api');
+  const socket = io('http://pomodoro.mikedanagam.space', {path: 'api'});
 
   socket.on('connect', () => {
     console.log('connected');
